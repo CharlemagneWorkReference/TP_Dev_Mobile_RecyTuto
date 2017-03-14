24a6d630f17d49be06621614fa3b0274077b4397
Cyprien Aubry <cyprienaubry@linkpulsion.com>
2017-03-14 11:22:55 +0100

    First commit mamene

---
+++ app/src/main/java/com/oneday_online/todolist/TodoContract.java
@@ -0,0 +1,12 @@
+package com.oneday_online.todolist;
+
+import android.provider.BaseColumns;
+
+public final class TodoContract {
+    public static class TodoEntry implements BaseColumns {
+        public static final String TABLE_NAME = "items";
+        public static final String COLUMN_NAME_LABEL = "label";
+        public static final String COLUMN_NAME_TAG = "tag";
+        public static final String COLUMN_NAME_DONE = "done";
+    }
+}
